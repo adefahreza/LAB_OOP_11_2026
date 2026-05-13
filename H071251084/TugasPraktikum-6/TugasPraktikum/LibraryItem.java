a4a139bce23bcbb4ebbb832a6e22047bd0cc7ed6
Ade Fahreza <andimuhammadadefahrezaarif@mac-adeezar.local>
2026-05-13 15:48:19 -0700

    Tambah Tugas Praktikum 6

---
+++ H071251084/TugasPraktikum-6/TugasPraktikum/LibraryItem.java
@@ -0,0 +1,22 @@
+package Tuprak6;
+
+abstract class LibraryItem {
+    String title;
+    int itemId;
+    boolean isBorrowed;
+
+    public LibraryItem(String title, int itemId, boolean isBorrowed) {
+        this.title = title;
+        this.itemId = itemId;
+        this.isBorrowed = isBorrowed;
+    }
+
+    abstract String getDescription();
+    abstract String borrowItem(int days);
+    abstract double calculateFine(int daysLate);
+
+    String returnItem() {
+        isBorrowed = false;
+        return title + " dikembalikan";
+    }
+}
