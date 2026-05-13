957ab05356304a210b3da0de0463394d97f500c1
Ade Fahreza <andimuhammadadefahrezaarif@mac-adeezar.local>
2026-05-13 16:59:48 -0700

    menambahkan Tuprak ke 7

---
+++ H071251084/TugasPraktikum-7/Soal2/DataProcessor.java
@@ -0,0 +1,12 @@
+package Soal2;
+
+import java.util.Random;
+
+class DataProcessor {
+    int process(String fileName) throws InterruptedException {
+        Random random = new Random();
+        int waktuProses = 500 + random.nextInt(1501); 
+        Thread.sleep(waktuProses);
+        return 100 + random.nextInt(901);
+    }
+}
---
+++ H071251084/TugasPraktikum-7/Soal2/DetailHasil.java
@@ -0,0 +1,13 @@
+package Soal2;
+
+class DetailHasil {
+    int jumlahKata;
+    String namaThread;
+    long durasi;
+
+    DetailHasil(int jumlahKata, String namaThread, long durasi) {
+        this.jumlahKata = jumlahKata;
+        this.namaThread = namaThread;
+        this.durasi = durasi;
+    }
+}
